e3302e6e4595d445d993eebfc5973c25f0911875
David Saah <dasorange.hope@gmail.com>
2024-01-24 16:01:04 +0000

    feat: add addition method

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ class ComplexNumber {
     ComplexNumber(double real, double imaginary);
     void display();
     ComplexNumber multiply(ComplexNumber& num);
+    ComplexNumber addition(ComplexNumber& num);
     double getReal();
     double getImaginary();
 
@@ -22,7 +23,11 @@ ComplexNumber::ComplexNumber(double real, double imaginary) {
 }
 
 void ComplexNumber::display() {
-  cout << real << " + " << imaginary << "i" << endl;
+  if (imaginary > 0) {
+    cout << real << " + " << imaginary << "i" << endl;
+  } else {
+    cout << real << " - " << -1 * imaginary << "i" << endl;
+  }
 }
 
 double ComplexNumber::getReal() {
@@ -41,7 +46,7 @@ ComplexNumber ComplexNumber::multiply(ComplexNumber& num) {
   /* img * img = real * (-1) */
 
   // operation to consider
-  /* (real + img) * (real + img) = (real * real) + (real * img) + (img * real)  + (img * img * (-1))*/
+  /* (real + img) * (real + img) = (real * real) + (real * img) + (img * real)  + (img * img * (-1)) */
   
   // group the real number outputs
   double realResult = (this->getReal() * num.getReal()) - (this->getImaginary() * num.getImaginary());
@@ -52,16 +57,30 @@ ComplexNumber ComplexNumber::multiply(ComplexNumber& num) {
   return ComplexNumber(realResult, imaginaryResult);
 }
 
+ComplexNumber ComplexNumber::addition(ComplexNumber& num) {
+  // operation to consider
+  /* (real + img) + (real + img) = (real + real) + (img + img) */
+  double realResult = this->getReal() + num.getReal();
+  double ImaginaryResult = this->getImaginary() + num.getImaginary();
+
+  return ComplexNumber(realResult, ImaginaryResult);
+}
+
+
 int main() {
   ComplexNumber num1(2, 3);
   ComplexNumber num2(4, 5);
-  ComplexNumber num3 = num1.multiply(num2);
 
   cout << "Complex number 1: ";
   num1.display();
   cout << "Complex number 2: ";
   num2.display();
 
+  ComplexNumber num3 = num1.multiply(num2);
   cout << "The result after multiplication: ";
   num3.display();
+
+  ComplexNumber num4 = num1.addition(num2);
+  cout << "The result after multiplication: ";
+  num4.display();
 }
